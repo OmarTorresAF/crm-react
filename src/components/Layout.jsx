e5c1d891523eea21046678a9e673b1b755cc3365
TDevtechs <torresdevtech@gmail.com>
2024-01-25 08:59:00 -0600

    minor Fix on tailwind.config.js

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,15 +2,15 @@ import { Outlet } from 'react-router-dom'
 
 function Layout () {
   return (
-        <div>
-            <h1 className="text-6xl font-bold">CRM - React</h1>
-            <h1 className="text-6xl font-bold">CRM - React</h1>
-            <h1 className="text-6xl font-bold">CRM - React</h1>
+       <div className='md:flex md:min-h-screen'>
+            <aside className='px-5 py-10 bg-blue-900 md:w-1/4'>
+                <h2 className='text-4xl font-black text-center text-white'>crm clientes</h2>
+            </aside>
 
-          <Outlet
-
-            />
-        </div>
+            <main className='p-10 overflow-scroll md:w-3/4 md:h-screen'>
+              <Outlet />
+            </main>
+       </div>
   )
 }
 
