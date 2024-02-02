74b32b72723b4f9abc35617076dbcb7c56f00ff4
TDevtechs <torresdevtech@gmail.com>
2024-02-02 15:45:03 -0600

    Add Action function

--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,11 +1,19 @@
-import { useNavigate } from 'react-router-dom'
+import { useNavigate, Form } from 'react-router-dom'
 import Formulario from './Formulario'
+
+export async function action ({ request }) {
+  const formData = await request.formData()
+
+  return { ok: true }
+}
+
+// el return de arriba es necesario ya que en nuevas versiones de react esto ha sido implementado como requisito https://reactrouter.com/en/main/route/action
 
 function NuevoCliente () {
   const navigate = useNavigate()
 
   return (
-    <>
+      <>
           <h1 className="text-4xl font-black text-blue-900">Nuevo Cliente</h1>
           <p className="mt-3">llena todos los campos para registrar un nuevo campo</p>
           <div className="flex justify-end">
@@ -17,16 +25,20 @@ function NuevoCliente () {
             </button>
           </div>
 
-          <form>
-                <div className='px-5 py-10 mx-auto mt-20 bg-white rounded-md shadow md:w-3/4'>
+          <div className='px-5 mx-auto bg-white rounded-md shadow md:w-3/4 py10 mt20'>
+
+        <Form
+          method='post' noValidate
+        >
                     <Formulario/>
                     <input
                         type='submit'
                         className='w-full p-3 mt-5 text-lg font-bold text-white uppercase bg-blue-800'
                         value='Registrar Cliente'
-                    />
-                </div>
-          </form>
+                     />
+
+        </Form>
+          </div>
     </>
   )
 }
