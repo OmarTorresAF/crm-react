908a812f9716c37aae30782052cd819c0e42b70e
TDevtechs <torresdevtech@gmail.com>
2024-02-03 11:25:15 -0600

    Change Formulario to the correct Folder, also added Error to the email

---
+++ src/components/Error.jsx
@@ -0,0 +1,9 @@
+function Error ({ children }) {
+  return (
+    <div className="my-4 font-bold text-center text-white uppercase bg-red-600 p3">
+          {children}
+    </div>
+  )
+}
+
+export default Error
--- src/pages/NuevoCliente.jsx
+++ src/pages/NuevoCliente.jsx
@@ -1,17 +1,39 @@
-import { useNavigate, Form } from 'react-router-dom'
-import Formulario from './Formulario'
+import { useNavigate, Form, useActionData } from 'react-router-dom'
+import Formulario from '../components/Formulario'
+import Error from '../components/Error'
 
 export async function action ({ request }) {
   const formData = await request.formData()
 
-  return { ok: true }
-}
+  const datos = Object.fromEntries(formData)
 
+  const email = formData.get('email')
+  // validacion
+
+  const errores = []
+
+  if (Object.values(datos).includes('')) {
+    errores.push('Todos los campos son obligatorios')
+  }
+
+  const regex = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])")
+  if (!regex.test(email)) {
+    errores.push('El email no es valido')
+  }
+
+  // RETORNAR DATOS SI HAY ERRORES
+  if (Object.keys(errores).length) {
+    return errores
+  }
+  // { return { ok: true }
+}
 // el return de arriba es necesario ya que en nuevas versiones de react esto ha sido implementado como requisito https://reactrouter.com/en/main/route/action
 
 function NuevoCliente () {
+  const errores = useActionData()
   const navigate = useNavigate()
 
+  console.log(errores)
   return (
       <>
           <h1 className="text-4xl font-black text-blue-900">Nuevo Cliente</h1>
@@ -26,6 +48,8 @@ function NuevoCliente () {
           </div>
 
           <div className='px-5 mx-auto bg-white rounded-md shadow md:w-3/4 py10 mt20'>
+
+          {errores?.length && errores.map((error, i) => <Error key={i}>{error}</Error>)}
 
         <Form
           method='post' noValidate
