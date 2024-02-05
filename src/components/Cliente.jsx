5be7e4a18a6747ce5a6d3806cf439985557f2d9e
TDevtechs <torresdevtech@gmail.com>
2024-02-05 10:48:00 -0600

    Added more button

--- src/components/Cliente.jsx
+++ src/components/Cliente.jsx
@@ -1,30 +1,35 @@
-function Cliente ({ cliente }) {
-  const { nombre, empresa, email, telefono, id } = cliente
+function Cliente({ cliente }) {
+    const { nombre, empresa, email, telefono, id } = cliente
 
-  return (
-      <tr className="border-b">
-          <td className='p-6 space-y-2'>
-              <p className="text-2xl text-gray-800"> {nombre} </p>
-              <p className="">{empresa}</p>
-          </td>
-          <td className="p-6 ">
-              <p className="text-gray-600 "> <span className="font-bold text-gray-800 uppercase">Email: </span> {email} </p>
-              <p className="text-gray-600 "> <span className="font-bold text-gray-800 uppercase">Telefono: </span> {telefono} </p>
-          </td>
-          <td className="flex gap-3 p-6">
-            <button
-            type="button"
-            className="text-xs font-bold text-blue-600 uppercase hover:text-blue-700"
-            >Editar</button>
-              <button
-                  type="button"
-                  className="text-xs font-bold text-red-600 uppercase hover:text-red-700"
-              >Eliminar</button>
-              <button>hola</button>
-          </td>
+    return (
+        <tr className="border-b">
+            <td className='p-6 space-y-2'>
+                <p className="text-2xl text-gray-800"> {nombre} </p>
+                <p className="">{empresa}</p>
+            </td>
+            <td className="p-6 ">
+                <p className="text-gray-600 "> <span className="font-bold text-gray-800 uppercase">Email: </span> {email} </p>
+                <p className="text-gray-600 "> <span className="font-bold text-gray-800 uppercase">Telefono: </span> {telefono} </p>
+            </td>
+            <td className="flex gap-3 p-6">
+                <button
+                    type="button"
+                    className="text-xs font-bold text-blue-600 uppercase hover:text-blue-700"
+                >Editar</button>
+                <button
+                    type="button"
+                    className="text-xs font-bold text-red-600 uppercase hover:text-red-700"
+                >Eliminar</button>
+                <button>hola</button>
+                <button>hola</button>
+                <button>hola</button>
+                <button>hola</button>
+                <button>hola</button>
+                <button>hola</button>
+            </td>
 
-      </tr>
-  )
+        </tr>
+    )
 }
 
 export default Cliente
